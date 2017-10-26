64d95b6f4a79b5c294f4338713aee03d08f01850
Raghu Ramarao <raghu.ramarao@gmail.com>
2017-10-26 18:05:05 +0530

    Final Submission

--- README.md
+++ README.md
@@ -1,36 +1,75 @@
 # Semantic Segmentation
 ### Introduction
-In this project, you'll label the pixels of a road in images using a Fully Convolutional Network (FCN).
+The objective of the project is to label the pixels of a road in images as either road/not-road by implementing a Fully Convolutional Network for semantic segmentation. This classification will help other systems in the car determine where the free space is. This technique could be extended to more classes like road, vehicle, bicycle and pedestrian.
+
+In this implementation, MobileNet is used for a series of convolutional layers to extract features (encoder).
+
+
+### A bit about Fully Convolutional Network
+Traditional convolutional network consists of series of convolutional layers, flattening convolutional layers followed by fully connected layers and ultimately a softmax activation function for classification. Flattening the convolutional layer destroys the spatial information. Hence, these networks are not capable of locating objects in an image. 
+
+Fully convolutional network (FCN) retains the spatial information throughout the network. Output is of the same size as the input image. This is done by,
+
+- replacing fully connected layers by 1x1 convolutional layers
+- upsampling through transpose convolutional layers
+- using skip connections
+
+Structurally, an FCN comprises of two parts; encoder and decoder.
+
+- The encoder is a series of convolutional layers like VGG and ResNet. The goal of the encoder is to extract features from the image.
+- The decoder upscales the output of the encoder such that it is the same size as the original image.
+
 
 ### Setup
+
 ##### Frameworks and Packages
 Make sure you have the following is installed:
  - [Python 3](https://www.python.org/)
  - [TensorFlow](https://www.tensorflow.org/)
  - [NumPy](http://www.numpy.org/)
  - [SciPy](https://www.scipy.org/)
+
 ##### Dataset
 Download the [Kitti Road dataset](http://www.cvlibs.net/datasets/kitti/eval_road.php) from [here](http://www.cvlibs.net/download.php?file=data_road.zip).  Extract the dataset in the `data` folder.  This will create the folder `data_road` with all the training a test images.
 
-### Start
-##### Implement
-Implement the code in the `main.py` module indicated by the "TODO" comments.
-The comments indicated with "OPTIONAL" tag are not required to complete.
-##### Run
+
+### How to run
+
+Project files include: 
+ - `helper.py`
+ - `main.py`
+ - `project_tests.py`
+ - Training data in `data` folder
+ - Trained model is saved in `models` folder
+ - Newest inference images from `runs` folder  (**all images from the most recent run**)
+
 Run the following command to run the project:
 ```
 python main.py
 ```
-**Note** If running this in Jupyter Notebook system messages, such as those regarding test status, may appear in the terminal rather than the notebook.
 
-### Submission
-1. Ensure you've passed all the unit tests.
-2. Ensure you pass all points on [the rubric](https://review.udacity.com/#!/rubrics/989/view).
-3. Submit the following in a zip file.
- - `helper.py`
- - `main.py`
- - `project_tests.py`
- - Newest inference images from `runs` folder  (**all images from the most recent run**)
- 
- ## How to write a README
-A well written README file can enhance your project and portfolio.  Develop your abilities to create professional README files by completing [this free course](https://www.udacity.com/course/writing-readmes--ud777).
+### Model Documentation
+
+##### Approach
+The project guideline reccomends to use pre-trained VGG for the encoder. The download is a huge file and is about ~ 550 MB. The project is implemented on a laptop with 4GB RAM, 2GB GPU (nvidia geforce 840M) and Ubuntu 16.04. Considering the hardware constraints, it was not feasible to load VGG weights. Instead, MobileNet (~ 70 MB) is used as an alternative. 
+
+MobileNets are small, low-latency, low-power models parameterized to meet the resource constraints of a variety of use cases. MobileNets are not usually as accurate as the bigger, more resource-intensive networks. But finding that resource/accuracy trade-off is where MobileNets really shine.
+
+##### Training Parameters
+
+- Number of epochs: 40
+- Optimizer: Adam
+- Generalization:
+    - Data augmentation
+    - Batch normalization - did not yield good results
+    - L2 regularisation with weight decay of 0.0004
+
+##### Model Graph
+
+![sem_seg_graph](https://user-images.githubusercontent.com/17127066/32052087-b3b3955e-ba74-11e7-9979-8244e3be4f06.png)
+
+### Video Implementation
+
+Developed pipeline is applied to generate a video. The project demo can be found at Youtube here.
+
+[![semantic_segmentation](https://user-images.githubusercontent.com/17127066/31305361-b0feafd4-ab55-11e7-891c-afc6da568bec.png)](https://www.youtube.com/watch?v=I4aXmthGsys)
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os.path
 import tensorflow as tf
+from tensorflow.contrib import slim
 import helper
 import warnings
 from distutils.version import LooseVersion
 import project_tests as tests
+import numpy as np
+
+import scipy.misc
+from moviepy.editor import VideoFileClip
+import mobilenet_v1 as mnet
 
 
 # Check TensorFlow Version
@@ -17,7 +23,7 @@ else:
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 
 
-def load_vgg(sess, vgg_path):
+def load_vgg(sess):
     """
     Load Pretrained VGG Model into TensorFlow.
     :param sess: TensorFlow Session
@@ -26,28 +32,87 @@ def load_vgg(sess, vgg_path):
     """
     # TODO: Implement function
     #   Use tf.saved_model.loader.load to load the model and weights
-    vgg_tag = 'vgg16'
-    vgg_input_tensor_name = 'image_input:0'
-    vgg_keep_prob_tensor_name = 'keep_prob:0'
-    vgg_layer3_out_tensor_name = 'layer3_out:0'
-    vgg_layer4_out_tensor_name = 'layer4_out:0'
-    vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
-tests.test_load_vgg(load_vgg, tf)
+    input_tensor_name = 'image_input:0'
+    layer3_out_tensor_name = 'MobilenetV1/MobilenetV1/Conv2d_5_pointwise/Relu6:0'
+    layer4_out_tensor_name = 'MobilenetV1/MobilenetV1/Conv2d_8_pointwise/Relu6:0'
+    layer7_out_tensor_name = 'MobilenetV1/MobilenetV1/Conv2d_13_pointwise/Relu6:0'
+    
+    graph = sess.graph
+
+    #for i in graph.get_operations():
+    #    print (i.name)
+
+    image_input = graph.get_tensor_by_name(input_tensor_name)
+    layer3_out = graph.get_tensor_by_name(layer3_out_tensor_name)
+    layer4_out = graph.get_tensor_by_name(layer4_out_tensor_name)
+    layer7_out = graph.get_tensor_by_name(layer7_out_tensor_name) 
+    
+    return image_input, layer3_out, layer4_out, layer7_out
+#tests.test_load_vgg(load_vgg, tf)
 
 
-def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
+def layers(layer3_out, layer4_out, layer7_out, num_classes):
     """
-    Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
-    :param vgg_layer7_out: TF Tensor for VGG Layer 3 output
-    :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
-    :param vgg_layer3_out: TF Tensor for VGG Layer 7 output
+    Create the layers for a fully convolutional network.  Build skip-layers using the mobilenet layers.
+    :param layer7_out: TF Tensor for Layer 3 output
+    :param layer4_out: TF Tensor for Layer 4 output
+    :param layer3_out: TF Tensor for Layer 7 output
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
+
+    ## fcn layers
+    print ("layer3_out",layer3_out.get_shape().as_list())
+    print ("layer4_out",layer4_out.get_shape().as_list())
+    print ("layer7_out",layer7_out.get_shape().as_list())
+
+    layer3_1x1 = tf.layers.conv2d(layer3_out, num_classes, 1, strides=(1,1), padding="SAME", name="layer3_fcn", 
+                               kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    print ("layer3_1x1",layer3_1x1.get_shape().as_list())
+
+    layer4_1x1 = tf.layers.conv2d(layer4_out, num_classes, 1, strides=(1,1), padding="SAME", name="layer4_fcn", 
+                               kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    print ("layer4_1x1",layer4_1x1.get_shape().as_list())
+
+    layer7_1x1 = tf.layers.conv2d(layer7_out, num_classes, 1, strides=(1,1), padding="SAME", name="layer7_fcn", 
+                               kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    print ("layer7_1x1",layer7_1x1.get_shape().as_list())
+
+    # Upscale 1
+    layer7_up = tf.layers.conv2d_transpose(layer7_1x1, num_classes, 4, strides=(2, 2), padding="SAME", name="layer7_up", 
+                                           kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    print ("layer7_up",layer7_up.get_shape().as_list())
+
+    # Add Skip Connections 1
+    # make sure the shapes are the same!
+    layer7_skip = tf.add(layer7_up, layer4_1x1, name="layer7_skip")
+
+    print ("layer7_skip",layer7_skip.get_shape().as_list())
+
+    # Upscale 2
+    layer4_up = tf.layers.conv2d_transpose(layer7_skip, num_classes, 4, strides=(2, 2), padding="SAME", name="layer4_up",  
+                                           kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    print ("layer4_up",layer4_up.get_shape().as_list())
+
+    # Add Skip Connections 2
+    # make sure the shapes are the same!
+    layer4_skip = tf.add(layer4_up, layer3_1x1, name="layer4_skip")
+
+    print ("layer4_skip",layer4_skip.get_shape().as_list())
+
+    # Upscale 3
+    layer3_up = tf.layers.conv2d_transpose(layer4_skip, num_classes, 16, strides=(8, 8), padding="SAME", name="layer3_up",  
+                                           kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    print ("layer3_up",layer3_up.get_shape().as_list())
+
+    return layer3_up
 tests.test_layers(layers)
 
 
@@ -61,8 +126,35 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, [-1, num_classes])
+    labels = tf.reshape(correct_label, [-1, num_classes])
+    
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits))
+
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
+
+
+def augment_data(images, gt_images):
+    """Augument data by some image transformation,
+    return pair of image patches..
+    Only support horizontal flipping for now
+    """
+    # flipping horizontally
+    hf_images = images[:, :, ::-1, :]
+    hf_gt_images = gt_images[:, :, ::-1, :]
+
+    # flipping vertically - it looks a little weird, but the intention
+    # is to force the model focus on patterns such as texture, than shapes 
+    # or orientations 
+    ## Experiments show that it doesn't really help too much
+    vf_images = images[:, ::-1, :, :]
+    vf_gt_images = gt_images[:, ::-1, :, :]
+
+    aug_images = np.concatenate([vf_images, images, hf_images], axis=0)
+    aug_gt_images = np.concatenate([vf_gt_images, gt_images, hf_gt_images], axis=0)
+    return aug_images, aug_gt_images
 
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
@@ -81,27 +173,62 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    pass
+    for epoch in range(epochs):
+        batches = get_batches_fn(batch_size)
+        for b, (seed_images, seed_gt_images) in enumerate(batches):
+            
+            ## for the intrusive test `tests.test_train_nn`
+            if seed_images.ndim == 4:
+                images, gt_images = augment_data(seed_images, seed_gt_images)
+            else: # for `tests.test_train_nn(train_nn)`
+                images, gt_images = seed_images, seed_gt_images
+
+            _, loss_val = sess.run([train_op, cross_entropy_loss],
+                                    feed_dict={input_image: images,
+                                               correct_label: gt_images,
+                                               keep_prob: 0.5,
+                                               learning_rate: 1e-4 })
+            if b % 10 == 0:
+                print("epoch %i batch %i loss=%.3f" % (epoch, b, loss_val))
+
 tests.test_train_nn(train_nn)
 
 
 def run():
     num_classes = 2
     image_shape = (160, 576)
-    data_dir = './data'
+    n_epochs = 40    # 5
+    batch_size = 4
+    data_dir = './data2'
     runs_dir = './runs'
     tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
-    helper.maybe_download_pretrained_vgg(data_dir)
+    # helper.maybe_download_pretrained_vgg(data_dir)
 
     # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
+
+    is_training = False
+    weight_decay = 0.0004
+    arg_scope = mnet.mobilenet_v1_arg_scope(is_training=is_training, weight_decay=weight_decay)
+
+    inp = tf.placeholder(tf.float32, shape=(None, None, None, 3), name="image_input")
+    keep_prob = 0.5
+    
+    with slim.arg_scope(arg_scope):
+        logits, _ = mnet.mobilenet_v1(inp, dropout_keep_prob=keep_prob, is_training=is_training)
+
+    rest_var = slim.get_variables_to_restore()
+
+    #print (rest_var)
+
+
     with tf.Session() as sess:
         # Path to vgg model
-        vgg_path = os.path.join(data_dir, 'vgg')
+        #vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
         get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
 
@@ -109,14 +236,70 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
-
+        
+        saver = tf.train.Saver(rest_var)
+        saver.restore(sess, './data2/vgg/mobilenet_v1_1.0_224.ckpt')
+        print ('Restored')
+        
+        input_image, layer3_out, layer4_out, layer7_out = load_vgg(sess)
+        
         # TODO: Train NN using the train_nn function
+        model_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
 
-        # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        target = tf.placeholder(dtype=tf.float32, shape=[None, None, None, num_classes])
+
+        learning_rate = tf.placeholder(dtype=tf.float32)
+        keep_prob = tf.placeholder(dtype=tf.float32)
+        
+        logits, train_op, loss = optimize(model_output, target, learning_rate, num_classes)
+        
+        sess.run(tf.global_variables_initializer())
+        
+        train_nn(sess, n_epochs, batch_size, get_batches_fn, 
+                 train_op, loss, input_image, target, keep_prob, learning_rate)
+        
+        saver = tf.train.Saver()
+        saver.save(sess, "./models/model.ckpt")
+        saver.export_meta_graph("./models/model.meta")
+        tf.train.write_graph(sess.graph_def, "./models/", "model.pb", False)
+
+        # Save inference data using helper.save_inference_samples
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
+        def process(im):
+
+            image = scipy.misc.imresize(im, image_shape)
+
+            im_softmax = sess.run([tf.nn.softmax(logits)], {keep_prob: 1.0, input_image: [image]})
+            im_softmax = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
+            segmentation = (im_softmax > 0.5).reshape(image_shape[0], image_shape[1], 1)
+            mask = np.dot(segmentation, np.array([[0, 255, 0, 127]]))
+            mask = scipy.misc.toimage(mask, mode="RGBA")
+            street_im = scipy.misc.toimage(image)
+            street_im.paste(mask, box=None, mask=mask)
+
+            return np.array(street_im)
+
+
+        white_output = 'project_out.mp4'
+        clip1 = VideoFileClip("project_video.mp4")
+        white_clip = clip1.fl_image(process) #NOTE: this function expects color images!!
+        white_clip.write_videofile(white_output, audio=False)
+
+        ## Tensorboard Logs
+        #writer = tf.summary.FileWriter("./tensorboard/1")
+        #writer.add_graph(sess.graph)
+        
+        # From command prompt
+        # tensorboard --logdir "./tensorboard/1"
+
+
 
 
 if __name__ == '__main__':
+    
     run()
+
+
+
